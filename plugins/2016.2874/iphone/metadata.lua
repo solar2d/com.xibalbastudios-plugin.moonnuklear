10c3d3de7dd7357c28714321f9e0d6d93e14eee0
ggcrunchy <steve@xibalbastudios.com>
2022-11-02 14:57:03 -0500

    First go at moonnuklear

---
+++ plugins/2016.2874/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'plugin_libmoonnuklear' },
+		frameworks = {},
+		frameworksOptional = {},
+	},
+}
+
+return metadata
